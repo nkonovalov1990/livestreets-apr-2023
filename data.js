ffa85703e98130ee12ad8a06bbe81615d79ed8fd
nkonovalov1990 <n.konovalov1990@gmail.com>
2023-04-11 19:19:00 +0400

    commit

---
+++ data.js
@@ -0,0 +1,17 @@
+const PLANS = [
+  {
+    title: 'Проект 2013 года',
+    map: 'amundsena_obezdnaya_2013.jpg',
+    old: true 
+  },
+  {
+    title: 'Проект Живых улиц',
+    map: 'amundsena_obezdnaya_predlozheniya_zhivykh_ulicz.jpg',
+    default: true
+  },
+  {
+    title: 'Ситуация',
+    map: 'amundsena_obezdnaya_iskhodnaya_situacziya.jpg',
+    old: true
+  }
+];
